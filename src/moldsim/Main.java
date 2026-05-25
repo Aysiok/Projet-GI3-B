a7fba49237ead276967cf4e3320e85b7eee2dc9f
Neimad1612 <damien.dimartino25@gmail.com>
2026-05-25 21:46:10 +0200

    ajout du foyer manuel

--- src/moldsim/Main.java
+++ src/moldsim/Main.java
@@ -1,23 +1,34 @@
 package moldsim;
-
 import moldsim.model.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Test Simulation Console ===");
-
         Environment env = new Environment();
         env.setHumidity(85.0);
         
         Grid grid = new Grid(10, 10, false, NeighborhoodMode.FOUR);
-        grid.getCell(5, 5).infect(MoldSpecies.CLADOSPORIUM);
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez la coordonnée X du foyer (0-9) : ");
+        int x = scanner.nextInt();
+        System.out.print("Entrez la coordonnée Y du foyer (0-9) : ");
+        int y = scanner.nextInt();
+
+        if (grid.inBounds(x, y)) {
+            grid.getCell(x, y).infect(MoldSpecies.CLADOSPORIUM);
+            System.out.println("Foyer placé en (" + x + "," + y + ")");
+        } else {
+            System.out.println("Coordonnées invalides, foyer placé au centre par défaut.");
+            grid.getCell(5, 5).infect(MoldSpecies.CLADOSPORIUM);
+        }
+        scanner.close();
         
         Simulation sim = new Simulation(grid, env);
-
         System.out.println("Etat initial :");
         printGrid(grid);
-
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 10; i++) {
             sim.step();
             System.out.println("Tour " + (i+1) + " :");
             printGrid(grid);
